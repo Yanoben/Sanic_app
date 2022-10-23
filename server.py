0b8f19a5952384c798eab6cc70d75ac7bbf93632
Ghost <yasinabdurakhmanov@gmail.com>
2022-10-23 16:48:09 +0300

    changes create models

--- server.py
+++ server.py
@@ -1,12 +1,39 @@
+from databases import Database
 from sanic import Sanic
 from sanic.response import json
 
-app = Sanic("my-hello-world-app")
+app = Sanic("sanic_app")
+
+
+def setup_database():
+    app.db = Database('postgresql://postgres:postgres@localhost/postgres')
+
+    @app.listener('after_server_start')
+    async def connect_to_db(*args, **kwargs):
+        await app.db.connect()
+
+    @app.listener('after_server_stop')
+    async def disconnect_from_db(*args, **kwargs):
+        await app.db.disconnect()
 
 
 @app.route('/')
 async def test(request):
     return json({'hello': 'world'})
 
+
+# @app.get("/products")
+# async def products(request):
+#     products = [
+#         {"title": "product_a", "price": 10.0},
+#         {"title": "product_b", "price": 5.0},
+#     ]
+#     return json(products)
+
+
 if __name__ == '__main__':
-    app.run()
+    setup_database()
+    app.run(host='0.0.0.0',
+            port=8000,
+            debug=True,
+            auto_reload=True,)
